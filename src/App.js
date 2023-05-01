29295460859bdfa5da4a88b7314b47984487f0ea
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-05-01 21:00:14 +0100

    UPDATE: static data to reusable mapped content

--- src/App.js
+++ src/App.js
@@ -3,9 +3,52 @@ import Header from "./components/Header/Header"
 import "./styles/styles.scss"
 
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Soup'
+    },
+    {
+      id: 2,
+      title: 'Salad'
+    },
+    {
+      id: 3,
+      title: 'Fast Food'
+    },
+    {
+      id: 4,
+      title: 'Oriental'
+    },
+    {
+      id: 5,
+      title: 'Indian'
+    },
+    {
+      id: 6,
+      title: 'Desert'
+    },
+    {
+      id: 6,
+      title: 'Drinks'
+    }
+  ]
+
   return (
     <div className="app">
       <Header />
+      {
+        categories.map(({title, id}) => (
+          <div key={id} className="category-container">
+            <div className="category-body-container">
+              <h2>
+                {title}
+              </h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))
+      }
       <Footer />
     </div>
   )
