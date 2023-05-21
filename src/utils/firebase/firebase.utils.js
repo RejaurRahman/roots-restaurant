8c9b7eb6f8811edc2fe16299f98dcdf0657e044d
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-05-22 00:18:38 +0100

    ADD: firebase integrated to store user login data

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,14 +5,20 @@ import {
   signInWithPopup,
   GoogleAuthProvider
 } from "firebase/auth"
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhYhEP3e-_yJsqNDUhMJ-jKlZK3z_K3ok",
-  authDomain: "roots-restaurant-db.firebaseapp.com",
-  projectId: "roots-restaurant-db",
-  storageBucket: "roots-restaurant-db.appspot.com",
-  messagingSenderId: "121764942320",
-  appId: "1:121764942320:web:8f3726e1f73d68ff014d35"
+  apiKey: "AIzaSyB2t0BPl426uop7EnLHWsEmISRknFbnM1c",
+  authDomain: "capsone-ecommerce.firebaseapp.com",
+  projectId: "capsone-ecommerce",
+  storageBucket: "capsone-ecommerce.appspot.com",
+  messagingSenderId: "966335920093",
+  appId: "1:966335920093:web:b03af4b318f81f4fb3a834"
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
@@ -25,3 +31,28 @@ provider.setCustomParameters({
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid)
+
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch (error) {
+      console.log("error creating the user", error.message)
+    }
+  }
+
+  return userDocRef
+}
