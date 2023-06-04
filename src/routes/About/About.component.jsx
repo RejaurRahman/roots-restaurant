782af0bec04c9cf7d20f3595ec0a98c4d3e71a0a
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-06-04 03:51:21 +0100

    ADD: text media for about us page

--- src/routes/About/About.component.jsx
+++ src/routes/About/About.component.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import PageBanner from "../../components/PageBanner/PageBanner.component"
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs.component"
+import TextMedia from "../../components/TextMedia/TextMedia.component"
 
 const About = () => {
   return (
     <>
       <PageBanner label="About" />
       <Breadcrumbs label="About" />
+      <TextMedia />
     </>
   )
 }
